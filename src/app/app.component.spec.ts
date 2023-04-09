cd239a680faaa0c57f258ef7d505d9ff20c9e880
Jimmy Murillo <salchichongallo1997@gmail.com>
2023-04-09 07:28:34 -0500

    chore: add Tailwind support

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,8 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'my-music-app app is running!',
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello world!');
   });
 });
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
