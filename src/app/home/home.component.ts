d7536f5cfb86aced19157a7fff01d863e79a21e8
Jimmy Murillo <salchichongallo1997@gmail.com>
2023-04-12 11:19:47 -0500

    style: remove unused files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,6 @@ import { Song, SongService } from '../song.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   username = '';
